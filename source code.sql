3d8d261d1dd0bb9f6409ebdd3a98f6e4a297266d
Tianen Cheng <92861361+ob-cheng@users.noreply.github.com>
2022-08-06 16:26:00 +0200

    Create source code.sql

---
+++ source code.sql
@@ -0,0 +1,1 @@
+--query to return the industry, the year, the number of companies in these industries that became unicorns each year in 2019, 2020, and 2021, along with the average valuation per industry per year, converted to billions of dollars and rounded to two decimal places--
