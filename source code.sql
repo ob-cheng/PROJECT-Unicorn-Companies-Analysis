695eb8096cf2d680794fe3eb2b2bee70fb21e159
Tianen Cheng <92861361+ob-cheng@users.noreply.github.com>
2022-08-06 22:45:21 +0200

    Update source code.sql

--- source code.sql
+++ source code.sql
@@ -1,4 +1,22 @@
+WITH top_industry as(
+SELECT i.industry, count(i.company_id) num_unicorns
+FROM industries i, dates d
+WHERE i.company_id = d.company_id AND EXTRACT(year from d.date_joined) in (2019, 2020, 2021)
+GROUP BY i.industry
+ORDER BY count(i.company_id) DESC
+LIMIT 3
+),
 
--- query to return the industry, the year, 
--- the number of companies in these industries that became unicorns each year in 2019, 2020, and 2021, 
--- along with the average valuation per industry per year, converted to billions of dollars and rounded to two decimal places
+add_info as(
+SELECT i.industry, EXTRACT(year from d.date_joined) "year", COUNT(i.company_id) num_unicorns, ROUND(avg(f.valuation)/1000000000,2) average_valuation_billions
+FROM industries i, dates d, funding f
+WHERE i.company_id = d.company_id AND d.company_id = f.company_id
+GROUP BY i.industry, EXTRACT(year from d.date_joined)
+)
+
+SELECT industry, year, num_unicorns, average_valuation_billions
+FROM add_info
+WHERE industry in (SELECT industry 
+                  FROM top_industry)
+                  AND year in (2019, 2020, 2021)
+ORDER BY industry, year DESC
